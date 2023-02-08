2674f48d0e38cc57501665430bd168418558ad7f
FedericoGossi <fgossi@student.ethz.ch>
2023-02-08 20:48:16 +0100

    Wrapper for the GEX to ADT prediction pipeline

---
+++ inst/python/prediction.py
@@ -0,0 +1,99 @@
+"""High level functions for running the model prediction pipeline."""
+from typing import Optional
+
+import anndata as ad
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+from preprocessing import GEXPreprocessor
+
+
+class ADTPredictor:
+    """Wrapper for running the GEX to ADT prediction pipeline."""
+
+    def __init__(
+            self,
+            do_log1p: Optional[bool] = True,
+            n_components: Optional[int] = 300,
+            do_tsvd_before_zscore: Optional[bool] = True,
+    ):
+        """
+        Parameters
+        ----------
+        do_log1p
+            Logarithmize data?
+            Default 'True' expects non-logarithmized data.
+        n_components
+            Number of components to use for truncated SVD.
+        do_tsvd_before_zscore
+            Perform truncated SVD before Z-score normalization?
+            Default 'True' works better for downstream training and prediction on data from a single dataset.
+            Set to 'False' to extract more robust features that work well across datasets.
+
+        """
+        self.gex_preprocessor = GEXPreprocessor(
+            do_log1p=do_log1p,
+            n_components=n_components,
+            do_tsvd_before_zscore=do_tsvd_before_zscore,
+        )
+        self.model = LinearRegression()
+
+    def fit(
+            self,
+            gex_train: np.ndarray,
+            adt_train: np.ndarray,
+            gex_test: Optional[np.ndarray] = None,
+    ):
+        """
+        Fit the GEX preprocessing and the GEX to ADT model to the training data.
+        gex_test is optional and is used for transductive preprocessing,
+        i.e. the truncated SVD is fit on the union of the training and test data.
+        Parameters
+        ----------
+        gex_train
+            Training GEX data.
+        adt_train
+            Training ADT data.
+        gex_test
+            Optional test GEX data for transductive preprocessing.
+        """
+        # Preprocess GEX data
+        gex_train = ad.AnnData(gex_train)
+        if gex_test is not None:
+            gex_test = ad.AnnData(gex_test)
+            gex_train_test = ad.concat((gex_train, gex_test), join='outer')
+            self.gex_preprocessor.fit_transform(gex_train_test)
+            X_train = gex_train_test.obsm['X_pca'][:gex_train.shape[0]]
+        else:
+            self.gex_preprocessor.fit_transform(gex_train)
+            X_train = gex_train.obsm['X_pca']
+
+        # Fit the model
+        self.model.fit(X_train, adt_train)
+
+    def predict(
+            self,
+            gex_test: np.ndarray,
+    ) -> np.ndarray:
+        """
+        Predict ADT from GEX.
+        Parameters
+        ----------
+        gex_test
+            Test GEX matrix.
+        Returns
+        -------
+        adt_pred
+            Predicted ADT matrix.
+        """
+        # Preprocess GEX data
+        gex_test = ad.AnnData(gex_test)
+        self.gex_preprocessor.transform(gex_test)
+        X_test = gex_test.obsm['X_pca']
+
+        # Predict ADT data
+        adt_pred = self.model.predict(X_test)
+        # Clip negative values
+        adt_pred = np.clip(adt_pred, a_min=0, a_max=None)
+
+        return adt_pred
--- inst/python/preprocessing.py
+++ inst/python/preprocessing.py
@@ -1,8 +1,9 @@
 """Preprocessing functions and recipes"""
-import scanpy as sc
-import numpy as np
-from anndata import AnnData
 from typing import Optional, List
+
+import numpy as np
+import scanpy as sc
+from anndata import AnnData
 from sklearn.decomposition import TruncatedSVD
 
 
@@ -27,46 +28,6 @@ def filter_to_common_genes(
     return adatas
 
 
-def recipe_lance22(
-        adata: AnnData,
-        n_tsvd_comp: Optional[int] = 300,
-        log: Optional[bool] = True,
-        copy: Optional[bool] = False,
-) -> Optional[AnnData]:
-    """
-    Normalization and filtering as of [Lance22]_.
-    Default `log=True` expects non-logarithmized data.
-    If using logarithmized data, pass `log=False`.
-    Parameters
-    ----------
-    adata
-        Annotated data matrix.
-    n_tsvd_comp
-        Number of components to use for truncated SVD.
-    log
-        Logarithmize data?
-    copy
-        Return a copy if true.
-    """
-    if copy:
-        adata = adata.copy()
-    # filter out duplicate variables
-    adata.var_names_make_unique()
-    # filter out non-expressed genes and cells
-    sc.pp.filter_genes(adata, min_counts=1)
-    sc.pp.filter_cells(adata, min_counts=1)
-    if log:
-        # log(1+x) transform
-        adata.X = sc.pp.log1p(adata.X)
-    # truncated SVD
-    tsvd = TruncatedSVD(n_components=300)
-    X_lowdim = tsvd.fit_transform(adata.X.toarray())
-    # scale data to unit variance and shift to zero mean (z-score normalization)
-    X_lowdim_norm = zscore_normalization(X_lowdim)
-    adata.obsm["X_pca"] = X_lowdim_norm
-    return adata if copy else None
-
-
 def zscore_normalization(
         X: np.ndarray
 ) -> np.ndarray:
@@ -81,3 +42,132 @@ def zscore_normalization(
     X_sd[X_sd == 0] = 1
     X_normalized = (X - np.mean(X, axis=1).reshape(-1, 1)) / X_sd
     return X_normalized.astype(np.float32)
+
+
+class GEXPreprocessor:
+    """
+    GEX preprocessing pipeline: log1p-transform,
+    filter non-expressed cells,
+    perform truncated SVD, and row-wise Z-score normalization.
+    """
+
+    def __init__(
+            self,
+            do_log1p: Optional[bool] = True,
+            n_components: Optional[int] = 300,
+            do_tsvd_before_zscore: Optional[bool] = True,
+    ):
+        """
+        Parameters
+        ----------
+        do_log1p
+            Log1p-transform data?
+            Default 'True' expects non-logarithmized data.
+        n_components
+            Number of components to use for truncated SVD.
+        do_tsvd_before_zscore
+            Perform truncated SVD before Z-score normalization?
+            Default 'True' works better for downstream training and prediction on data from a single dataset.
+            Set to 'False' to extract more robust features that work well across datasets.
+        """
+        self.do_log1p = do_log1p
+        self.tsvd = TruncatedSVD(n_components=n_components)
+        self.do_tsvd_before_zscore = do_tsvd_before_zscore
+
+    def _transform_before_tsvd(
+            self,
+            adata: AnnData,
+            copy: Optional[bool] = False,
+    ) -> AnnData:
+        """
+        Filter non-expressed cells,
+        optionally log1p-transform and z-score normalize.
+        Parameters
+        ----------
+        adata
+            Annotated data matrix.
+        copy
+            Copy adata before transforming?
+
+        Returns
+        -------
+        Returns the transformed adata.
+        """
+        if copy:
+            adata = adata.copy()
+        if self.do_log1p:
+            adata.X = sc.pp.log1p(adata.X)
+
+        # filter out non-expressed cells
+        sc.pp.filter_cells(adata, min_counts=1)
+
+        if not self.do_tsvd_before_zscore:
+            # row-wise Z-score normalization
+            adata.X = zscore_normalization(adata.X)
+        return adata
+
+    def fit(
+            self,
+            adata: AnnData,
+            copy: Optional[bool] = False,
+    ):
+        """
+        Fit the truncated SVD.
+        Default `copy=False` modifies the input AnnData.
+        Parameters
+        ----------
+        adata
+            Annotated GEX matrix.
+        copy
+            Copy adata before fitting?
+        """
+        self.fit_transform(adata, copy=copy)
+
+    def transform(
+            self,
+            adata: AnnData,
+            copy: Optional[bool] = False,
+    ) -> Optional[AnnData]:
+        """
+        Transform the GEX matrix.
+        Parameters
+        ----------
+        adata
+            Annotated GEX matrix.
+        copy
+            Return a copy if true.
+        Returns
+        -------
+        Returns or updates `adata`, depending on `copy`.
+        """
+        # filter non-expressed cells, optionally log1p-transform and z-score normalize
+        adata = self._transform_before_tsvd(adata, copy=copy)
+        X_lowdim = self.tsvd.transform(adata.X)
+        if self.do_tsvd_before_zscore:
+            X_lowdim = zscore_normalization(X_lowdim)
+        adata.obsm["X_pca"] = X_lowdim
+        return adata if copy else None
+
+    def fit_transform(
+            self,
+            adata: AnnData,
+            copy: Optional[bool] = False,
+    ) -> Optional[AnnData]:
+        """
+        Fit the truncated SVD and transform the GEX matrix.
+        Parameters
+        ----------
+        adata
+            Annotated GEX matrix.
+        copy
+            Return a copy if true.
+        Returns
+        -------
+        Returns or updates `adata`, depending on `copy`.
+        """
+        adata = self._transform_before_tsvd(adata, copy=copy)
+        X_lowdim = self.tsvd.fit_transform(adata.X)
+        if self.do_tsvd_before_zscore:
+            X_lowdim = zscore_normalization(X_lowdim)
+        adata.obsm["X_pca"] = X_lowdim
+        return adata if copy else None
