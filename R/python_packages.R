516cc51d5bf63527815ff9db5151e78927474d8e
Daniel Hanhart <daniel.hanhart@gmail.com>
2023-05-31 11:53:36 +0200

    31.05.2023

--- R/python_packages.R
+++ R/python_packages.R
@@ -9,8 +9,8 @@ preprocessing <- NULL
 .onLoad <- function(libname, pkgname){
     reticulate::configure_environment(pkgname)
     module_path <-  base::system.file("python",package = "scLinear")
-    numpy <<- reticulate::import("numpy", delay_load = TRUE)
     scanpy <<- reticulate::import("scanpy", delay_load = TRUE)
+    numpy <<- reticulate::import("numpy", delay_load = TRUE)
     joblib <<- reticulate::import("joblib", delay_load = TRUE)
     preprocessing <<- reticulate::import_from_path(module = "preprocessing", path = module_path, delay_load = TRUE)
     prediction <<- reticulate::import_from_path(module = "prediction", path = module_path, delay_load = TRUE)
