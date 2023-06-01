9214ff4bf4efdfa6c171e7d448d290bdddd1e094
Daniel Hanhart <daniel.hanhart@gmail.com>
2023-06-01 17:30:42 +0200

    01.06.2023

--- R/sclinear.R
+++ R/sclinear.R
@@ -211,7 +211,7 @@ adt_predict <- function(pipe, gexp, normalize = TRUE){
 
   adt_assay <- Seurat::CreateAssayObject(data = adt)
 
-  Seurat::Key(adt_assay) <- "predictedadt_"
+  #Seurat::Key(adt_assay) <- "predictedadt_"
 
   return(adt_assay)
 }
