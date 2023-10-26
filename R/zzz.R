21f10b2e2c60c44cf39430f688c4b81fa13cb5ba
Daniel Hanhart <daniel.hanhart@gmail.com>
2023-10-26 22:29:41 +0200

    update zzz.R file

--- R/zzz.R
+++ R/zzz.R
@@ -14,14 +14,13 @@ scanpy <- NULL
 .onLoad <- function(libname, pkgname){
     reticulate::configure_environment(pkgname)
     module_path <-  base::system.file("python",package = "scLinear")
-    scanpy <<- reticulate::import("scanpy", delay_load = TRUE)
     numpy <<- reticulate::import("numpy", delay_load = TRUE)
     joblib <<- reticulate::import("joblib", delay_load = TRUE)
+    torch <<- reticulate::import_from_path(module = "torch", delay_load = TRUE)
+    pytorch_lightning <<- reticulate::import_from_path(module = "pytorch_lightning", delay_load = TRUE)
+    sklearn <<- reticulate::import_from_path(module = "sklearn", delay_load = TRUE)
+    scanpy <<- reticulate::import("scanpy", delay_load = TRUE)
     preprocessing <<- reticulate::import_from_path(module = "preprocessing", path = module_path, delay_load = TRUE)
     prediction <<- reticulate::import_from_path(module = "prediction", path = module_path, delay_load = TRUE)
     evaluate <<- reticulate::import_from_path(module = "evaluate", path = module_path, delay_load = TRUE)
-    torch <<- reticulate::import_from_path(module = "torch", path = module_path, delay_load = TRUE)
-    pytorch_lightning <<- reticulate::import_from_path(module = "pytorch_lightning", path = module_path, delay_load = TRUE)
-    sklearn <<- reticulate::import_from_path(module = "sklearn", path = module_path, delay_load = TRUE)
-
     }
