2ac6f44f14a66a9e5d3d46ef8f3e97b74c2e2833
DanHanh <daniel.hanhart@gmail.com>
2023-11-02 20:16:45 +0100

    update zzz.R file, prediction.py file change to work with zzz.R

--- inst/python/prediction.py
+++ inst/python/prediction.py
@@ -15,225 +15,225 @@ from torch.utils.data import TensorDataset, DataLoader
 #from babel_models import VanillaNN, IdentityTransformer, BabelDance
 from preprocessing import GEXPreprocessor
 
-class ADTPredictor:
-    """Wrapper for running the GEX to ADT prediction pipeline."""
-
-    def __init__(
-            self,
-            do_log1p: Optional[bool] = False,
-            n_components: Optional[int] = 300,
-            do_tsvd_before_zscore: Optional[bool] = True,
-    ):
-        """
-        Parameters
-        ----------
-        do_log1p
-            Logarithmize data?
-            Default 'False' expects logarithmized data.
-        n_components
-            Number of components to use for truncated SVD.
-        do_tsvd_before_zscore
-            Perform truncated SVD before Z-score normalization?
-            Default 'True' works better for downstream training and prediction on data from a single dataset.
-            Set to 'False' to extract more robust features that work well across datasets.
-
-        """
-        self.gex_preprocessor = GEXPreprocessor(
-            do_log1p=do_log1p,
-            n_components=n_components,
-            do_tsvd_before_zscore=do_tsvd_before_zscore,
-        )
-        self.model = LinearRegression()
-        self.gex_names = None
-        self.adt_names = None
-
-    def fit(
-            self,
-            gex_train: np.ndarray,
-            adt_train: np.ndarray,
-            gex_test: Optional[np.ndarray] = None,
-            gex_names: Optional[np.ndarray] = None,
-            adt_names: Optional[np.ndarray] = None,
-    ):
-        """
-        Fit the GEX preprocessing and the GEX to ADT model to the training data.
-        gex_test is optional and is used for transductive preprocessing,
-        i.e. the truncated SVD is fit on the union of the training and test data.
-        Parameters
-        ----------
-        gex_train
-            Training GEX data.
-        adt_train
-            Training ADT data.
-        gex_test
-            Optional test GEX data for transductive preprocessing.
-        gex_names
-            Optional GEX gene names.
-        adt_names
-            Optional ADT protein names.
-        """
-        # If GEX or ADT names are provided, save them
-        # ADT names will be returned in the prediction
-        if gex_names is not None:
-            self.gex_names = gex_names
-        if adt_names is not None:
-            self.adt_names = adt_names
-        # Preprocess GEX data
-        gex_train = ad.AnnData(gex_train, dtype=gex_train.dtype)
-
-        if gex_test is not None:
-            gex_test = ad.AnnData(gex_test, dtype=gex_test.dtype)
-            with warnings.catch_warnings():
-                # ignore UserWarning: Observation names are not unique.
-                warnings.filterwarnings("ignore", category=UserWarning)
-                gex_train_test = ad.concat((gex_train, gex_test), join='outer')
-            self.gex_preprocessor.fit_transform(gex_train_test)
-            X_train = gex_train_test.obsm['X_pca'][:gex_train.shape[0]]
-        else:
-            self.gex_preprocessor.fit_transform(gex_train)
-            X_train = gex_train.obsm['X_pca']
-
-        # Fit the model
-        self.model.fit(X_train, adt_train)
-
-    def _filter_gex_names(
-            self,
-            gex_test: np.ndarray,
-            gex_names: np.ndarray,
-    ) -> np.ndarray:
-        if self.gex_names is None:
-            raise ValueError(
-                'GEX names were not provided during training. '
-                'Please provide GEX names during training or prediction '
-                'if you want to match gene names by providing gex_names as an argument.'
-            )
-        if not np.array_equal(gex_names, self.gex_names):
-            # Discard the genes that are not in the training data
-            # Set the genes that are in the test data but not in the training data to 0
-            # And sort the columns to match the training data
-            selfgex2idx = dict()
-            for i, g in enumerate(self.gex_names):
-                selfgex2idx[g] = i
-            gex_test_new = np.zeros((gex_test.shape[0], len(self.gex_names)))
-            for i, g in enumerate(gex_names):
-                if g in selfgex2idx:
-                    gex_test_new[:, selfgex2idx[g]] = gex_test[:, i]
-            gex_test = gex_test_new
-        return gex_test
-
-    def predict(
-            self,
-            gex_test: np.ndarray,
-            gex_names: Optional[np.ndarray] = None,
-    ) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Predict ADT from GEX.
-        Parameters
-        ----------
-        gex_test
-            Test GEX matrix.
-        gex_names
-            Optional GEX gene names of the columns of `gex_test`.
-            If provided, the function will check if `gex_names` matches `self.gex_names` used for training,
-            and will use only the columns that match.
-            The names in `gex_names` that do are not in `self.gex_names` will be ignored,
-            and the columns of `gex_test` that do not have a matching name in `gex_names` will be set to 0.
-        Returns
-        -------
-        adt_pred
-            Predicted ADT matrix.
-        adt_names
-            ADT protein names of the prediction.
-        """
-        # If GEX names are provided, check if they match the training names
-        if gex_names is not None:
-            gex_test = self._filter_gex_names(gex_test, gex_names)
-        # Preprocess GEX data
-        gex_test = ad.AnnData(gex_test, dtype=gex_test.dtype)
-        self.gex_preprocessor.transform(gex_test)
-        X_test = gex_test.obsm['X_pca']
-
-        # Predict ADT data
-        adt_pred = self.model.predict(X_test)
-        # Clip negative values
-        adt_pred = np.clip(adt_pred, a_min=0, a_max=None)
-
-        return adt_pred, self.adt_names
-
-    def feature_importance(self, gex_test: np.ndarray, gex_names: Optional[np.ndarray] = None) -> np.ndarray:
-        """
-        Get the feature importance of the GEX genes for predicting the ADT proteins.
-        Parameters
-        ----------
-        gex_test
-            Test GEX matrix.
-        gex_names
-            Optional GEX gene names of the columns of `gex_test`.
-            If provided, the function will check if `gex_names` matches `self.gex_names` used for training,
-            and will use only the columns that match.
-            The names in `gex_names` that do are not in `self.gex_names` will be ignored,
-            and the columns of `gex_test` that do not have a matching name in `gex_names` will be set to 0.
-        Returns
-        -------
-        feature_importance
-            Feature importance matrix of dimensions #ADT * #GEX,
-            it's the Jacobian matrix of the ADT prediction with respect to the GEX input,
-            averaged over all the cells in gex_test.
-        """
-        if not self.gex_preprocessor.do_tsvd_before_zscore:
-            raise NotImplementedError('Feature importance calculation is not implemented for this case')
-
-        # If GEX names are provided, check if they match the training names and filter them
-        if gex_names is not None:
-            gex_test = self._filter_gex_names(gex_test, gex_names)
-
-        # Preprocess GEX data
-        gex_test = ad.AnnData(gex_test, dtype=gex_test.dtype)
-        self.gex_preprocessor.transform(gex_test)
-        X_test = gex_test.obsm['X_pca']
-
-        # Calculate the Jacobian of the z-scored low-dim GEX w.r.t. the low-dim GEX using PyTorch >=2.0
-        from torch.func import vmap, jacrev
-        def zscore_torch(X):
-            return (X - X.mean()) / torch.sqrt(torch.var(X, correction=0))
-        X_torch = torch.tensor(X_test)
-        J = vmap(jacrev(zscore_torch))(X_torch)
-
-        # Calculate the feature importance as W(adt*comps) * J(comps*comps) * V(comps*gex)
-        WJ = torch.tensor(self.model.coef_) @ J
-        feature_importance = WJ @ torch.tensor(self.gex_preprocessor.tsvd.components_)
-
-        # Average the feature importance over all the cells
-        return feature_importance.mean(axis=0).numpy()
-
-
-    def save(self, path: str, compress: Optional[bool] = True):
-        """
-        Save the trained pipeline to a file.
-        Parameters
-        ----------
-        path
-            Path to the file.
-        compress
-            Whether to compress the file, default True.
-        """
-        import joblib
-        joblib.dump(self, path, compress=compress)
-
-    def load(self, path: str):
-        """
-        Load a pretrained pipeline from a file.
-        Parameters
-        ----------
-        path
-            Path to the file.
-        """
-        import joblib
-        pretrained_pipe = joblib.load(path)
-        self.gex_preprocessor = pretrained_pipe.gex_preprocessor
-        self.model = pretrained_pipe.model
-        self.gex_names = pretrained_pipe.gex_names
-        self.adt_names = pretrained_pipe.adt_names
+# class ADTPredictor:
+#     """Wrapper for running the GEX to ADT prediction pipeline."""
+# 
+#     def __init__(
+#             self,
+#             do_log1p: Optional[bool] = False,
+#             n_components: Optional[int] = 300,
+#             do_tsvd_before_zscore: Optional[bool] = True,
+#     ):
+#         """
+#         Parameters
+#         ----------
+#         do_log1p
+#             Logarithmize data?
+#             Default 'False' expects logarithmized data.
+#         n_components
+#             Number of components to use for truncated SVD.
+#         do_tsvd_before_zscore
+#             Perform truncated SVD before Z-score normalization?
+#             Default 'True' works better for downstream training and prediction on data from a single dataset.
+#             Set to 'False' to extract more robust features that work well across datasets.
+# 
+#         """
+#         self.gex_preprocessor = GEXPreprocessor(
+#             do_log1p=do_log1p,
+#             n_components=n_components,
+#             do_tsvd_before_zscore=do_tsvd_before_zscore,
+#         )
+#         self.model = LinearRegression()
+#         self.gex_names = None
+#         self.adt_names = None
+# 
+#     def fit(
+#             self,
+#             gex_train: np.ndarray,
+#             adt_train: np.ndarray,
+#             gex_test: Optional[np.ndarray] = None,
+#             gex_names: Optional[np.ndarray] = None,
+#             adt_names: Optional[np.ndarray] = None,
+#     ):
+#         """
+#         Fit the GEX preprocessing and the GEX to ADT model to the training data.
+#         gex_test is optional and is used for transductive preprocessing,
+#         i.e. the truncated SVD is fit on the union of the training and test data.
+#         Parameters
+#         ----------
+#         gex_train
+#             Training GEX data.
+#         adt_train
+#             Training ADT data.
+#         gex_test
+#             Optional test GEX data for transductive preprocessing.
+#         gex_names
+#             Optional GEX gene names.
+#         adt_names
+#             Optional ADT protein names.
+#         """
+#         # If GEX or ADT names are provided, save them
+#         # ADT names will be returned in the prediction
+#         if gex_names is not None:
+#             self.gex_names = gex_names
+#         if adt_names is not None:
+#             self.adt_names = adt_names
+#         # Preprocess GEX data
+#         gex_train = ad.AnnData(gex_train, dtype=gex_train.dtype)
+# 
+#         if gex_test is not None:
+#             gex_test = ad.AnnData(gex_test, dtype=gex_test.dtype)
+#             with warnings.catch_warnings():
+#                 # ignore UserWarning: Observation names are not unique.
+#                 warnings.filterwarnings("ignore", category=UserWarning)
+#                 gex_train_test = ad.concat((gex_train, gex_test), join='outer')
+#             self.gex_preprocessor.fit_transform(gex_train_test)
+#             X_train = gex_train_test.obsm['X_pca'][:gex_train.shape[0]]
+#         else:
+#             self.gex_preprocessor.fit_transform(gex_train)
+#             X_train = gex_train.obsm['X_pca']
+# 
+#         # Fit the model
+#         self.model.fit(X_train, adt_train)
+# 
+#     def _filter_gex_names(
+#             self,
+#             gex_test: np.ndarray,
+#             gex_names: np.ndarray,
+#     ) -> np.ndarray:
+#         if self.gex_names is None:
+#             raise ValueError(
+#                 'GEX names were not provided during training. '
+#                 'Please provide GEX names during training or prediction '
+#                 'if you want to match gene names by providing gex_names as an argument.'
+#             )
+#         if not np.array_equal(gex_names, self.gex_names):
+#             # Discard the genes that are not in the training data
+#             # Set the genes that are in the test data but not in the training data to 0
+#             # And sort the columns to match the training data
+#             selfgex2idx = dict()
+#             for i, g in enumerate(self.gex_names):
+#                 selfgex2idx[g] = i
+#             gex_test_new = np.zeros((gex_test.shape[0], len(self.gex_names)))
+#             for i, g in enumerate(gex_names):
+#                 if g in selfgex2idx:
+#                     gex_test_new[:, selfgex2idx[g]] = gex_test[:, i]
+#             gex_test = gex_test_new
+#         return gex_test
+# 
+#     def predict(
+#             self,
+#             gex_test: np.ndarray,
+#             gex_names: Optional[np.ndarray] = None,
+#     ) -> Tuple[np.ndarray, np.ndarray]:
+#         """
+#         Predict ADT from GEX.
+#         Parameters
+#         ----------
+#         gex_test
+#             Test GEX matrix.
+#         gex_names
+#             Optional GEX gene names of the columns of `gex_test`.
+#             If provided, the function will check if `gex_names` matches `self.gex_names` used for training,
+#             and will use only the columns that match.
+#             The names in `gex_names` that do are not in `self.gex_names` will be ignored,
+#             and the columns of `gex_test` that do not have a matching name in `gex_names` will be set to 0.
+#         Returns
+#         -------
+#         adt_pred
+#             Predicted ADT matrix.
+#         adt_names
+#             ADT protein names of the prediction.
+#         """
+#         # If GEX names are provided, check if they match the training names
+#         if gex_names is not None:
+#             gex_test = self._filter_gex_names(gex_test, gex_names)
+#         # Preprocess GEX data
+#         gex_test = ad.AnnData(gex_test, dtype=gex_test.dtype)
+#         self.gex_preprocessor.transform(gex_test)
+#         X_test = gex_test.obsm['X_pca']
+# 
+#         # Predict ADT data
+#         adt_pred = self.model.predict(X_test)
+#         # Clip negative values
+#         adt_pred = np.clip(adt_pred, a_min=0, a_max=None)
+# 
+#         return adt_pred, self.adt_names
+# 
+#     def feature_importance(self, gex_test: np.ndarray, gex_names: Optional[np.ndarray] = None) -> np.ndarray:
+#         """
+#         Get the feature importance of the GEX genes for predicting the ADT proteins.
+#         Parameters
+#         ----------
+#         gex_test
+#             Test GEX matrix.
+#         gex_names
+#             Optional GEX gene names of the columns of `gex_test`.
+#             If provided, the function will check if `gex_names` matches `self.gex_names` used for training,
+#             and will use only the columns that match.
+#             The names in `gex_names` that do are not in `self.gex_names` will be ignored,
+#             and the columns of `gex_test` that do not have a matching name in `gex_names` will be set to 0.
+#         Returns
+#         -------
+#         feature_importance
+#             Feature importance matrix of dimensions #ADT * #GEX,
+#             it's the Jacobian matrix of the ADT prediction with respect to the GEX input,
+#             averaged over all the cells in gex_test.
+#         """
+#         if not self.gex_preprocessor.do_tsvd_before_zscore:
+#             raise NotImplementedError('Feature importance calculation is not implemented for this case')
+# 
+#         # If GEX names are provided, check if they match the training names and filter them
+#         if gex_names is not None:
+#             gex_test = self._filter_gex_names(gex_test, gex_names)
+# 
+#         # Preprocess GEX data
+#         gex_test = ad.AnnData(gex_test, dtype=gex_test.dtype)
+#         self.gex_preprocessor.transform(gex_test)
+#         X_test = gex_test.obsm['X_pca']
+# 
+#         # Calculate the Jacobian of the z-scored low-dim GEX w.r.t. the low-dim GEX using PyTorch >=2.0
+#         from torch.func import vmap, jacrev
+#         def zscore_torch(X):
+#             return (X - X.mean()) / torch.sqrt(torch.var(X, correction=0))
+#         X_torch = torch.tensor(X_test)
+#         J = vmap(jacrev(zscore_torch))(X_torch)
+# 
+#         # Calculate the feature importance as W(adt*comps) * J(comps*comps) * V(comps*gex)
+#         WJ = torch.tensor(self.model.coef_) @ J
+#         feature_importance = WJ @ torch.tensor(self.gex_preprocessor.tsvd.components_)
+# 
+#         # Average the feature importance over all the cells
+#         return feature_importance.mean(axis=0).numpy()
+# 
+# 
+#     def save(self, path: str, compress: Optional[bool] = True):
+#         """
+#         Save the trained pipeline to a file.
+#         Parameters
+#         ----------
+#         path
+#             Path to the file.
+#         compress
+#             Whether to compress the file, default True.
+#         """
+#         import joblib
+#         joblib.dump(self, path, compress=compress)
+# 
+#     def load(self, path: str):
+#         """
+#         Load a pretrained pipeline from a file.
+#         Parameters
+#         ----------
+#         path
+#             Path to the file.
+#         """
+#         import joblib
+#         pretrained_pipe = joblib.load(path)
+#         self.gex_preprocessor = pretrained_pipe.gex_preprocessor
+#         self.model = pretrained_pipe.model
+#         self.gex_names = pretrained_pipe.gex_names
+#         self.adt_names = pretrained_pipe.adt_names
 
 
 # class ADTPredictorKRREnsemble(ADTPredictor):
