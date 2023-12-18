fdddf1f7b5fb584651ca8b5882ee678ad19656ab
DanHanh <daniel.hanhart@gmail.com>
2023-12-18 13:35:42 +0100

    remove unused function

--- R/zzz.R
+++ R/zzz.R
@@ -203,37 +203,3 @@ evaluate <<- reticulate::import_from_path("evaluate",module_path,delay_load = TR
 prediction <<- reticulate::import_from_path("prediction",module_path,delay_load = TRUE)
 }
 
-
-
-
-
-
-
-#' Install all python dependencies for scLinear
-#'
-#' @return NULL
-#' @export
-#'
-#' @examples
-#' \dontrun{
-#' install_pyton_dependencies()
-#' }
-install_pyton_dependencies <- function(){
-  # Install python dependencies
-  if(any(
-    !all(sapply(c("numpy", "sklearn", "joblib", "anndata", "torch", "scanpy"), function(x){reticulate::py_module_available(x)}))
-  )){
-    install.dependencies <- readline('Do you want to install the necessary python packages (yes/no)?')
-    if(install.dependencies == "y" || install.dependencies == "yes"){
-      if(!reticulate::py_module_available("numpy")) suppressWarnings(suppressMessages(reticulate::conda_install(packages = "numpy")))
-      if(!reticulate::py_module_available("sklearn")) suppressWarnings(suppressMessages(reticulate::conda_install(packages = "scikit-learn")))
-      if(!reticulate::py_module_available("anndata")) suppressWarnings(suppressMessages(reticulate::conda_install(packages = "anndata")))
-      if(!reticulate::py_module_available("joblib")) suppressWarnings(suppressMessages(reticulate::conda_install(packages = "joblib")))
-      if(!reticulate::py_module_available("torch")) suppressWarnings(suppressMessages(reticulate::conda_install(packages = "pytorch-lightning")))
-      if(!reticulate::py_module_available("scanpy")) suppressWarnings(suppressMessages(reticulate::conda_install(packages = "scanpy")))
-    }
-  }else{
-    print("All python dependencies are already available!")
-    }
-
-}
